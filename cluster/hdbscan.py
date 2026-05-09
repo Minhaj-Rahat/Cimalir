bd1a741e7e88fceb3598fc8efc77c4a80064d8c0
Minhajul Alam Rahat <58078897+Minhaj-Rahat@users.noreply.github.com>
2026-05-09 14:57:33 -0600

    cleanup hdbscan.py

--- cluster/hdbscan.py
+++ cluster/hdbscan.py
@@ -1,143 +1,94 @@
-import numpy as np
 import pickle
 
-
-# merge yara dict and similarity matrix
-def merge_yara_similarity(sim_mat_file,yara_dict_file, sample_length, string_feature_length):
-    f_combine = open(sim_mat_file, 'rb')
-    similarity_dict_combined, _ = pickle.load(f_combine)   # the file contains similartiy dict and family name
-    f_combine.close()
-
-    f_yara = open(yara_dict_file, 'rb')
-    yara_dict = pickle.load(f_yara)
-    f_yara.close()
-
-    #final_feature_mat = np.zeros((sample_length+string_feature_length,sample_length+string_feature_length))
-    final_feature_mat = np.zeros((sample_length , sample_length + string_feature_length))
-    for i in range(sample_length):
-        for j in range(sample_length):
-            if (i,j) in similarity_dict_combined.keys():
-                final_feature_mat[i, j] = similarity_dict_combined[(i, j)] #* 100
-            else:
-                final_feature_mat[i, j] = similarity_dict_combined[(j, i)] #* 100
+import numpy as np
 
 
-    for i in range(sample_length):
-        final_feature_mat[i, sample_length:sample_length+string_feature_length] = yara_dict[i]
+def _load_pickle(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
 
 
-    return final_feature_mat
+def _build_similarity_block(similarity_dict, n):
+    """Densify `similarity_dict` into an n x n matrix.
 
-def merge_yara_similarity2(sim_mat_file,yara_dict_file, sample_length, string_feature_length):
-    f_combine = open(sim_mat_file, 'rb')
-    similarity_dict_combined, _ = pickle.load(f_combine)   # the file contains similartiy dict and family name
-    f_combine.close()
-
-    f_yara = open(yara_dict_file, 'rb')
-    yara_dict = pickle.load(f_yara)
-    f_yara.close()
-
-    final_feature_mat = np.zeros((sample_length,sample_length+string_feature_length))
-
-    for i in range(sample_length):
-        for j in range(sample_length):
-            if (i,j) in similarity_dict_combined.keys():
-                final_feature_mat[i, j] = similarity_dict_combined[(i, j)] #* 100
-            else:
-                final_feature_mat[i, j] = similarity_dict_combined[(j, i)] #* 100
+    For each (i, j), uses the score under (i, j) if present, otherwise (j, i).
+    """
+    mat = np.zeros((n, n))
+    for (i, j), v in similarity_dict.items():
+        if i < n and j < n:
+            mat[i, j] = v
+            if mat[j, i] == 0:
+                mat[j, i] = v
+    return mat
 
 
-    #for i in range(sample_length):
-        #final_feature_mat[i, sample_length:sample_length+string_feature_length] = yara_dict[i]
+def merge_yara_similarity(sim_mat_file, yara_dict_file, sample_length, string_feature_length):
+    similarity_dict_combined, _ = _load_pickle(sim_mat_file)
+    yara_dict = _load_pickle(yara_dict_file)
+
+    final = np.zeros((sample_length, sample_length + string_feature_length))
+    final[:, :sample_length] = _build_similarity_block(similarity_dict_combined, sample_length)
+    if string_feature_length:
+        for i in range(sample_length):
+            final[i, sample_length:sample_length + string_feature_length] = yara_dict[i]
+    return final
 
 
-    return final_feature_mat
+def merge_yara_similarity2(sim_mat_file, yara_dict_file, sample_length, string_feature_length):
+    """Variant that omits the yara block."""
+    similarity_dict_combined, _ = _load_pickle(sim_mat_file)
+    final = np.zeros((sample_length, sample_length + string_feature_length))
+    final[:, :sample_length] = _build_similarity_block(similarity_dict_combined, sample_length)
+    return final
 
 
-
-def hdbscan_cluster(yara_file,sim_mat, min_cluster_size=45,min_samples=20):
+def hdbscan_cluster(yara_file, sim_mat, min_cluster_size=45, min_samples=20):
     from collections import Counter
     import hdbscan
 
-    clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples, cluster_selection_method='eom', metric='euclidean',gen_min_span_tree=True) # current paper
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
+                                cluster_selection_method='eom', metric='euclidean',
+                                gen_min_span_tree=True)
 
-    mat = merge_main(yara_file,sim_mat) # current paper
-    #mat = merge_main2(yara_file) # without call, without strings
-
-    #mat = merge_main2(yara_file) # without strings
-
+    mat = merge_main(yara_file, sim_mat)
     cl = clusterer.fit(mat)
-
     labels1 = cl.labels_
     dbcv = cl.relative_validity_
 
+    _, family_name1 = _load_pickle('test_results/similarity_matrix')
 
-    result_file1 = 'test_results/similarity_matrix'  # similarity matrix file
+    families = ('ddos', 'gafgyt', 'tsunami', 'hajime', 'dofloo', 'mirai')
+    family1 = {fam: [] for fam in families}
+    for fam, label in zip(family_name1, labels1):
+        family1[fam].append(label)
 
-    # loading scores
-    f1 = open(result_file1, 'rb')
-    _, family_name1 = pickle.load(f1)
-
-
-    family1 = {}
-
-    family1['ddos'] = []
-    family1['gafgyt'] = []
-    family1['tsunami'] = []
-    family1['hajime'] = []
-    family1['dofloo'] = []
-    family1['mirai'] = []
-
-
-    result_dict1 = {}
-
-
-    for i in range(len(family_name1)):
-        family1[family_name1[i]].append(labels1[i])
-
-
-
-    for key in list(family1.keys()):
-        result_dict1[key] = dict(Counter(family1[key]))
+    result_dict1 = {fam: dict(Counter(labels)) for fam, labels in family1.items()}
     print(result_dict1)
-
     print(f'DVCV Index:{dbcv}')
 
-
-
-    family_dict = {'gafgyt':0,'tsunami':1,'ddos':2,'mirai':3,'hajime':4,'dofloo':5}
-
-    true_label = []
-
-    for i in range(len(family_name1)):
-        true_label.append(family_dict[family_name1[i]])
+    family_dict = {'gafgyt': 0, 'tsunami': 1, 'ddos': 2, 'mirai': 3, 'hajime': 4, 'dofloo': 5}
+    true_label = [family_dict[name] for name in family_name1]
 
     cluster_analyze(true_label, labels1, mat)
-
 
 
 def hdbscan_optimal(yara_file, sim_mat):
     import hdbscan
 
-
-    mat = merge_main(yara_file,sim_mat) # ours, current
-
-    #mat = merge_main2(yara_file) # without string features and without call
-
+    mat = merge_main(yara_file, sim_mat)
 
     best_score = 0
+    best_parameters = None
 
-    for min_cluster_size in [3, 5,10, 15, 20, 25, 30, 35, 40, 45,50, 55, 60, 65, 70, 75, 80]:
-        for min_samples in [3,5,10,20,30, 40, 45, 50, 55, 60,65,70,80,85]:
-            for cluster_selection_method in ['eom', 'leaf']:
-                for metric in ['euclidean']:
-                    # for each combination of parameters of hdbscan
-                    hdb = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
-                                          cluster_selection_method=cluster_selection_method, metric=metric,
-                                          gen_min_span_tree=True).fit(mat)
-                    # DBCV score
+    for min_cluster_size in (3, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80):
+        for min_samples in (3, 5, 10, 20, 30, 40, 45, 50, 55, 60, 65, 70, 80, 85):
+            for cluster_selection_method in ('eom', 'leaf'):
+                for metric in ('euclidean',):
+                    hdb = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size,
+                                          min_samples=min_samples,
+                                          cluster_selection_method=cluster_selection_method,
+                                          metric=metric, gen_min_span_tree=True).fit(mat)
                     score = hdb.relative_validity_
-                    # if we got a better DBCV, store it and the parameters
                     if score > best_score:
                         best_score = score
                         best_parameters = {'min_cluster_size': min_cluster_size,
@@ -150,31 +101,11 @@ def hdbscan_optimal(yara_file, sim_mat):
 
 
 def merge_main(yara_file, sim_mat_file):
-
-    # sim_mat_file = 'test_results/similarity_matrix' # current paper
-
-
-    yara_dict_file = yara_file
-    sample_length = 1000
-    string_length = 11
-
-    s = merge_yara_similarity(sim_mat_file, yara_dict_file, sample_length, string_length)
-    return s
-
-# without string features and without call
-def merge_main2(yara_file,sim_mat_file):
+    return merge_yara_similarity(sim_mat_file, yara_file, sample_length=1000, string_feature_length=11)
 
 
-    #sim_mat_file = 'test_results/similarity_matrix' # current system 1000 samples
-
-
-
-    yara_dict_file = yara_file #current system 1000 samples
-    sample_length = 1000
-    string_length = 0
-
-    s = merge_yara_similarity2(sim_mat_file, yara_dict_file, sample_length, string_length)
-    return s
+def merge_main2(yara_file, sim_mat_file):
+    return merge_yara_similarity2(sim_mat_file, yara_file, sample_length=1000, string_feature_length=0)
 
 
 def cluster_analyze(true_labels, pred_labels, mat):
@@ -183,20 +114,6 @@ def cluster_analyze(true_labels, pred_labels, mat):
     print(f"Homogeneity: {metrics.homogeneity_score(true_labels, pred_labels):.3f}")
     print(f"Completeness: {metrics.completeness_score(true_labels, pred_labels):.3f}")
     print(f"V-measure: {metrics.v_measure_score(true_labels, pred_labels):.3f}")
-    print(
-        "Adjusted Mutual Information:"
-        f" {metrics.adjusted_mutual_info_score(true_labels, pred_labels):.3f}"
-    )
-
-
-    print("Calinski Harabasz Score:"
-          f" {metrics.calinski_harabasz_score(mat, pred_labels):.3f}")
-
-    print("Silhouette Score:"
-          f" {metrics.silhouette_score(mat, pred_labels):.3f}")
-
-
-# Best DBCV score: 0.748
-# Best parameters: {'min_cluster_size': 45, ' min_samples': 20, 'cluster_selection_method': 'eom', 'metric': 'euclidean'} main system
-# Best parameters: {'min_cluster_size': 35, ' min_samples': 10, 'cluster_selection_method': 'eom', 'metric': 'euclidean'}  without strings, with call
-# Best parameters: {'min_cluster_size': 3, ' min_samples': 3, 'cluster_selection_method': 'eom', 'metric': 'euclidean'} without call
+    print(f"Adjusted Mutual Information: {metrics.adjusted_mutual_info_score(true_labels, pred_labels):.3f}")
+    print(f"Calinski Harabasz Score: {metrics.calinski_harabasz_score(mat, pred_labels):.3f}")
+    print(f"Silhouette Score: {metrics.silhouette_score(mat, pred_labels):.3f}")
