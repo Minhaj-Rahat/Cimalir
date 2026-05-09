40586e5800e7db046bf4e640fee214a6a773df30
Minhajul Alam Rahat <58078897+Minhaj-Rahat@users.noreply.github.com>
2026-05-09 14:50:32 -0600

    simplified regex_match_packer.py

--- utils/regex_match_packer.py
+++ utils/regex_match_packer.py
@@ -1,20 +1,7 @@
 import re
-#from Pkg.UPXUnpacker.Unpack import unpack
-
-"""
-search_string --- the string to march
-input_string -- given input string
-"""
 
 
 def find_string(search_string, input_string):
-
-    search_word = r"\b" + search_string + r"\b"
-
-    output = re.search(search_word, input_string, flags=re.IGNORECASE)
-
-    no_match = (output is None)
-    if no_match:
-        return False
-    else:
-        return True
+    """Whole-word, case-insensitive search for `search_string` in `input_string`."""
+    pattern = r"\b" + re.escape(search_string) + r"\b"
+    return re.search(pattern, input_string, flags=re.IGNORECASE) is not None
